6546c68f3cd5523e1bd04561436b780de38a2c9b
Fabio <Fabio@ada-school.com>
2024-09-30 21:08:46 -0300

    CRD Completo con MockAPI

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const obtenerProductos = async () => {
 
 const mostrarProductos = productos => {
   const lista = document.getElementById('productos-lista')
-
+  lista.innerHTML = ''
   productos.forEach(producto => {
     const productoArticle = document.createElement('article')
     productoArticle.classList.add('producto')
@@ -17,9 +17,40 @@ const mostrarProductos = productos => {
     <p>Precio: $ ${producto.precio}</p>
     <img src="${producto.imagen}" alt="imagen de ${producto.nombre}"/>
     <p>${producto.descripcion}</p>
+    <button onclick="eliminarProducto('${producto.id}')">Eliminar</button>
     `
     lista.appendChild(productoArticle)
   })
 }
+
+const eliminarProducto = async (id) => {
+  await fetch(`${API_URL}/${id}`, {
+    method: 'DELETE'
+  })
+  obtenerProductos()
+}
+
+const agregarProducto = async (producto) => {
+  await fetch(API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: producto ? JSON.stringify(producto) : null
+  })
+  obtenerProductos()
+}
+
+const form = document.getElementById('producto-form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const nuevoProducto = {
+    nombre: document.getElementById('nombre').value,
+    precio: document.getElementById('precio').value,
+    imagen: document.getElementById('imagen').value,
+    descripcion: document.getElementById('descripcion').value
+  }
+  agregarProducto(nuevoProducto)
+  form.reset()
+})
 
 obtenerProductos()
